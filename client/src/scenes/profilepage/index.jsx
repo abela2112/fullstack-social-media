edca3f745b8b1741fc5accc480ac2f2755471c76
abela2112 <abela9326@gmail.com>
2023-10-22 01:32:30 +0300

    update profilepage

--- client/src/scenes/profilepage/index.jsx
+++ client/src/scenes/profilepage/index.jsx
@@ -1,5 +1,6 @@
 import { Box, useMediaQuery } from "@mui/material"
 import axios from "axios"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import Navbar from "scenes/Navbar"
 import FriendsList from "scenes/widgets/FriendsList"
